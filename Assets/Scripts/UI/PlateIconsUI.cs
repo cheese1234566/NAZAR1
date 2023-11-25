592472b92ae9d97107e38877966ccc0870c5abb3
Cheese12 <2879nazar@gmail.com>
2023-11-25 10:57:43 +0200

    Add plate icons ui

--- Assets/Scripts/UI/PlateIconsUI.cs
+++ Assets/Scripts/UI/PlateIconsUI.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlateIconsUI : MonoBehaviour
 {
     [SerializeField] private PlateKitchenObject plateKitchenObject;
-
+    [SerializeField] private Transform iconTemplate;
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
     private void Start()
     {
         plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
@@ -13,6 +17,25 @@ public class PlateIconsUI : MonoBehaviour
 
     private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
     {
-        
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child == iconTemplate)
+            {
+                continue;
+            }
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+            Transform iconTransform = Instantiate(iconTemplate, transform);
+            iconTransform.GetComponent<PlateIconsSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+            iconTransform.gameObject.SetActive(true);
+        }
     }
 }
